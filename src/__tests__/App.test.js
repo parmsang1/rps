111ff76154ed02a02cead43e9f609a10ba4fe78f
Parminder Sanghera <sanghera.p@gmail.com>
2019-06-07 13:32:20 +0100

    app shows heading introducing game on homepage

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -4,5 +4,5 @@ import App from "../App";
 
 it("renders welcome message", () => {
   const { getByText } = render(<App />);
-  expect(getByText("Learn React")).toBeInTheDocument();
+  expect(getByText("Lets play rock, paper and scissors")).toBeInTheDocument();
 });
