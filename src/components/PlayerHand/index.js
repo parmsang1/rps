a0f3f86448fbf43bb3553f4878088420105070fc
Parminder Sanghera <sanghera.p@gmail.com>
2019-06-07 15:56:49 +0100

    make radio selection for PlayerHand a controlled component

--- src/components/PlayerHand/index.js
+++ src/components/PlayerHand/index.js
@@ -1,20 +1,42 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
 function PlayerHand(props) {
+  const [value, setValue] = useState("Rock");
+
+  const handleChange = event => setValue(event.target.value);
+
   return (
     <div data-testid="player-hand" className="player-hand">
       <form>
         <fieldset>
           <legend>Choose your hand</legend>
           <label>
-            Rock <input type="radio" name="radio1" value="Rock" />
+            Rock
+            <input
+              type="radio"
+              value="Rock"
+              checked={value === "Rock"}
+              onChange={handleChange}
+            />
           </label>
           <label>
-            Paper <input type="radio" name="radio1" value="Paper" />
+            Paper
+            <input
+              type="radio"
+              value="Paper"
+              checked={value === "Paper"}
+              onChange={handleChange}
+            />
           </label>
           <label>
-            Scissors <input type="radio" name="radio1" value="Scissors" />
+            Scissors
+            <input
+              type="radio"
+              value="Scissors"
+              checked={value === "Scissors"}
+              onChange={handleChange}
+            />
           </label>
         </fieldset>
       </form>
